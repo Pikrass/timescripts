8f4916f8d7db324255068fbd64ec8dd506d7dac6
Bastien Scher <bastien0705@gmail.com>
2013-05-04 23:00:50 +0200

    Add the clever spoiler script in the timescripts bundle

--- timescripts.user.js
+++ timescripts.user.js
@@ -2,10 +2,11 @@
 // @name Timescripts
 // @description Collection of scripts for the One True Thread
 // @author Pikrass
-// @version 1095
+// @version 1097
 // @require newpix-convertor.user.js
 // @require spoiler-detector.user.js
 // @require multiquote.user.js
+// @require clever-spoiler.user.js
 // @resource quote_waiting imgs/quote_waiting.png
 // @resource quote_ok imgs/quote_ok.png
 // @include http://forums.xkcd.com/viewtopic.php*
