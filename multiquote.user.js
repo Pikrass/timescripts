aa4377c922d9f25e6ee5a49f98ae3aec6d00ce7d
Bastien Scher <bastien0705@gmail.com>
2013-05-18 13:08:51 +0200

    Multiquote: add a text field when quoting to write the reply immediately
    Close #3

--- multiquote.user.js
+++ multiquote.user.js
@@ -2,7 +2,7 @@
 // @name Multiquote for the One True Forum
 // @description Changes quote buttons behaviour to quote multiple messages
 // @author Pikrass
-// @version 1094
+// @version 1423
 // @resource quote_waiting imgs/quote_waiting.png
 // @resource quote_ok imgs/quote_ok.png
 // @include http://forums.xkcd.com/viewtopic.php*
@@ -12,7 +12,13 @@
 // ==/UserScript==
 
 multiquote = {
+	init: function() {
+		this.quotes = JSON.parse(GM_getValue('quotes', '{}'));
+	},
+
 	convert: function() {
+		this.init();
+
 		var buttons = document.getElementsByClassName('quote-icon');
 		var i;
 		for(i=0 ; i<buttons.length ; i++) {
@@ -29,6 +35,10 @@ multiquote = {
 		req.open('get', link.quoteUrl, true);
 		req.send();
 		link.style.backgroundImage = 'url("'+GM_getResourceURL('quote_waiting')+'")';
+
+		var div = this.findAncestorByClass(link, 'postbody');
+		if(div != null && div.lastChild.className != 'multiquote-reply')
+			this.displayReplyArea(div);
 	},
 
 	addQuote: function(req, link) {
@@ -37,19 +47,71 @@ multiquote = {
 		var end = req.responseText.indexOf('</textarea>', beg);
 		var str = req.responseText.substring(beg, end);
 
-		var quote = GM_getValue('quoteText');
-		if(quote == undefined)
-			quote = str;
-		else
-			quote += "\n\n\n"+str;
+		var num = link.quoteUrl.match(/&p=(\d+)/)[1];
 
-		GM_setValue('quoteText', quote);
+		if(typeof this.quotes[num] == 'undefined') {
+			this.quotes[num] = {};
+		}
+
+		this.quotes[num].quote = str;
+
+		GM_setValue('quotes', JSON.stringify(this.quotes));
 		link.style.backgroundImage = 'url("'+GM_getResourceURL('quote_ok')+'")';
 	},
 
+	displayReplyArea: function(div) {
+		var post = this.findAncestorByClass(div, 'post');
+		var pId = post.id.substr(1);
+
+		var preDiv = document.createElement('div');
+		preDiv.appendChild(document.createTextNode('Reply:'));
+		preDiv.style.marginTop = '10px';
+		preDiv.style.fontSize = '1.3em';
+
+		var area = document.createElement('textarea');
+		area.className = 'multiquote-reply';
+		area.style.width = '100%';
+		area.style.height = '100px';
+		area.style.fontSize = '1.2em';
+
+		var butDiv = document.createElement('div');
+		butDiv.style.textAlign = 'right';
+		var but = document.createElement('input');
+		but.type = 'button';
+		but.value = 'Save';
+		but.style.fontWeight = 'bold';
+		but.addEventListener('click', this.addReply.bind(this, area, pId, but));
+		butDiv.appendChild(but);
+
+		area.addEventListener('keypress', this.onAreaChange.bind(this, area, but));
+
+		div.appendChild(preDiv);
+		div.appendChild(area);
+		div.appendChild(butDiv);
+	},
+
+	onAreaChange: function(area, but) {
+		but.value = 'Save';
+		but.disabled = false;
+	},
+
+	addReply: function(area, num, but) {
+		if(typeof this.quotes[num] == 'undefined') {
+			this.quotes[num] = {}; // This is *absolutely* thread-safe :D
+		}
+
+		this.quotes[num].reply = area.value;
+		GM_setValue('quotes', JSON.stringify(this.quotes));
+
+		but.value = 'Saved';
+		but.disabled = true;
+	},
+
 	dumpQuotes: function() {
+		this.init();
+
 		var tmpDiv = document.createElement('div');
-		tmpDiv.innerHTML = GM_getValue('quoteText', '');
+		tmpDiv.innerHTML = this.aggregateQuotes();
 		var str = tmpDiv.firstChild.data;
 
 		var area = document.getElementById('message');
@@ -64,7 +126,35 @@ multiquote = {
 	},
 
 	flush: function() {
-		GM_deleteValue('quoteText');
+		this.quotes = {};
+		GM_deleteValue('quotes');
+	},
+
+	aggregateQuotes: function() {
+		var s = '';
+		var i;
+		for(i in this.quotes) {
+			if(s != '')
+				s += "\n\n";
+
+			s += this.quotes[i].quote;
+
+			if(this.quotes[i].reply)
+				s += "\n" + this.quotes[i].reply;
+		}
+
+		return s;
+	},
+
+
+	findAncestorByClass: function(elem, className) {
+		if(new RegExp('\\b'+className+'\\b').test(elem.className))
+			return elem;
+		else {
+			if(elem != document.body)
+				return this.findAncestorByClass(elem.parentNode, className);
+			return null;
+		}
 	}
 };
 
